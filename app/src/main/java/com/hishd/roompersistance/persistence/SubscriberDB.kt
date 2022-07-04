91935159cd8bda78c9bb2237a6fe0dca252cbbdf
Hishara Dilshan <hish2k15@gmail.com>
2022-07-05 00:13:12 +0530

    Added Database Class

---
+++ app/src/main/java/com/hishd/roompersistance/persistence/SubscriberDB.kt
@@ -0,0 +1,31 @@
+package com.hishd.roompersistance.persistence
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+//Pass all the entities here
+@Database(entities = [Subscriber::class], version = 1)
+abstract class SubscriberDB: RoomDatabase() {
+    abstract val subscriberDAO: SubscriberDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: SubscriberDB? = null
+
+        fun getInstance(context: Context): SubscriberDB {
+            synchronized(this) {
+                var instance: SubscriberDB? = INSTANCE
+                if(instance==null) {
+                    instance = Room.databaseBuilder(
+                        context,
+                        SubscriberDB::class.java,
+                        "subscriber_db"
+                    ).build()
+                }
+                return instance
+            }
+        }
+    }
+}
